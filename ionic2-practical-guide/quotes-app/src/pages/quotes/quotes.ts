99f1b739679787163dacf51a653137bab85912dc
alltej <allan.tejano@gmail.com>
2017-01-28 08:03:10 -0500

    end of lecture 50 - quotes-app with mark as favorite (alertController, handler, log to console)

--- ionic2-practical-guide/quotes-app/src/pages/quotes/quotes.ts
+++ ionic2-practical-guide/quotes-app/src/pages/quotes/quotes.ts
@@ -1,22 +1,48 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component, OnInit} from '@angular/core';
+import {NavParams, AlertController} from 'ionic-angular';
+import {Quote} from "../../data/quote.interface";
 
-/*
-  Generated class for the Quotes page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html'
 })
-export class QuotesPage {
+export class QuotesPage implements OnInit{
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
+  ngOnInit(): void {
+     this.quoteGroup = this.navParams.data;
+   }
+
+  // ionViewDidLoad(){
+  //   this.quoteGroup = this.navParams.data;
+  //   Add elvis operator (?) in template to use this approach
+  // }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () =>{
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () =>{
+            console.log('Cancelled');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
-
 }
