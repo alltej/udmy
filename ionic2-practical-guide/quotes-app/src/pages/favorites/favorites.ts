1fd7bf6967a55f043c6c213ff9cb273e6331b03f
alltej <allan.tejano@gmail.com>
2017-01-28 09:57:13 -0500

    end of lecture 61 (remove favorites from service, passing data from modal to the page underlay)

--- ionic2-practical-guide/quotes-app/src/pages/favorites/favorites.ts
+++ ionic2-practical-guide/quotes-app/src/pages/favorites/favorites.ts
@@ -21,7 +21,23 @@ export class FavoritesPage {
   }
 
   onViewQuote(quote: Quote){
-    const model = this.modalCtrl.create(QuotePage, quote);
-    model.present();
+    const modal = this.modalCtrl.create(QuotePage, quote);
+    modal.present();
+     modal.onDidDismiss((remove: boolean) =>{
+       if ( remove){
+         this.quotesService.removeQuoteToFavorites(quote);
+         // alt 1: reload the quotes array from service
+         //this.quotes = this.quotesService.getFavoriteQuotes();
+         // alt 2. remove quote from array
+         const position = this.quotes.findIndex((quoteEl: Quote) =>{
+           return quoteEl.id == quote.id;
+         })
+         this.quotes.splice(position, 1);
+       }
+       //console.log('D ' + remove);
+     });
+    // modal.willLeave.subscribe(
+    //   (remove: boolean) => console.log('S ' + remove)
+    // )
   }
 }
